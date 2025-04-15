d41f204bf437b1e904584b3cbf5a0b8c3884e924
afanyuij <afanyuij@gmail.com>
2025-04-15 10:36:59 +0200

    started with course model

--- project-root/backend/server.js
+++ project-root/backend/server.js
@@ -99,6 +99,28 @@ app.put("/updateuser", async(req,res)=>{
     res.status(200).json({"message": "user updated successfully"})
 })
 
+// courses schema
+
+const courseSchema =new mongoose.Schema({
+    // title, description ,instructor,price, thumbnail,[modules: title, enum: video pdf contenturl ]
+    "title": {type:String, required:true },
+    "description": {type:String, required:true },
+    "instructor": { type: con.Schema.Types.ObjectId, ref: "Users" },
+    "price": {type:Number},
+    "thumbnail":{type:String}, // image in base64
+    "modules": [
+        {
+            "title": {type:String, required:true},
+            "type": {type:String, enum:["video", "pdf"]},
+            "contentUrl":{type:String}
+        }
+    ]
+
+
+})
+const courseModel = mongoose.model("Courses", courseSchema)
+
+
 
 
 app.listen(3000, ()=>{
